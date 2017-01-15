71821bdb3461ebdfaa9606c60fd95a2641fa0db6
shefferit <shefferit@WS42.cs.colman.ac.il>
2017-01-15 14:55:15 +0200

    asd

--- Sokoban_v1/src/boot/Run.java
+++ Sokoban_v1/src/boot/Run.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import controller.CLI;
 
 public class Run {
-////
+////ss
 	public static void main(String[] args) throws IOException {
 		CLI l=new CLI();
 		l.receiveInput();
